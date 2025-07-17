cb308ca3051e4aa3cbd6b9278384db75abf17c48
meiyume <93853843+meiyume@users.noreply.github.com>
2025-07-17 22:24:18 +0800

    Update streamlit_ta_mini.py

--- streamlit_ta_mini.py
+++ streamlit_ta_mini.py
@@ -12,7 +12,7 @@ from llm_utils import call_llm
 st.set_page_config(page_title="Technical Analyst AI Agent", page_icon="🌍")
 st.markdown("""
 <h1 style='margin-bottom: 0.3em;'>Technical Analyst AI Agent 🤖</h1>
-<h4 style='margin-top: 0;'>Global Macro</h4>
+<h1 style='margin-top: 0;'>Global Macro</h1>
 """, unsafe_allow_html=True)
 
 st.markdown(
